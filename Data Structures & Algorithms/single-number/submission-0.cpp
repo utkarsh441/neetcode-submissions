c75ba6f5c64710f4ee71ce936d9e8eabefc8f4d6
Utkarsh Srivastava <utkarsh.srivastava9281@gmail.com>
2026-07-23 23:27:53 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xori = 0;
+        for(int i = 0; i < nums.size(); i++) xori ^= nums[i];
+        return xori;
+    }
+};
